c7d0d3055466f58f6679fe45368d33050b4785e4
huxinghuas <dllo@huxinghua.local>
2016-09-08 19:39:34 +0800

    创建工程

---
+++ GiftSay/app/src/main/java/activity/lanou3g/com/giftsay/MainActivity.java
@@ -0,0 +1,13 @@
+package activity.lanou3g.com.giftsay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
