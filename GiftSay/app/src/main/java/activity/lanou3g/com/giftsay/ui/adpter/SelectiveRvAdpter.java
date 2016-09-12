ff0b9315a1f64665c6e3a9c9b2b8fb2b929f7f74
huxinghuas <dllo@huxinghua.local>
2016-09-12 11:13:48 +0800

    精选页面 recycle view 图片 横向滑动

---
+++ GiftSay/app/src/main/java/activity/lanou3g/com/giftsay/ui/adpter/SelectiveRvAdpter.java
@@ -0,0 +1,72 @@
+package activity.lanou3g.com.giftsay.ui.adpter;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import activity.lanou3g.com.giftsay.R;
+
+
+/**
+ * Created by dllo on 16/9/12.
+ * 首页->精品->recycleview-->适配器
+ */
+public class SelectiveRvAdpter  extends  RecyclerView.Adapter<SelectiveRvAdpter.SelectiveRvHolder> {
+
+    private List<Integer> datas;
+    private Context context;
+
+    public SelectiveRvAdpter(List<Integer> datas, Context context) {
+        this.datas = datas;
+        this.context = context;
+    }
+
+    @Override
+    public SelectiveRvHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.item_selective_rv, parent, false);
+        SelectiveRvHolder holder = new SelectiveRvHolder(view);
+        return holder;
+
+    }
+
+
+    @Override
+    public void onBindViewHolder(SelectiveRvHolder holder, int position) {
+          holder.img.setImageResource(datas.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return datas != null && datas.size() > 0 ? datas.size() : 0;
+    }
+
+    class SelectiveRvHolder extends RecyclerView.ViewHolder {
+
+        private ImageView img;
+
+        public SelectiveRvHolder(View itemView) {
+            super(itemView);
+            img = (ImageView) itemView.findViewById(R.id.item_selective_img);
+            Log.d("xxx", "shitu");
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
--- GiftSay/app/src/main/java/activity/lanou3g/com/giftsay/ui/fragment/SelectiveFragment.java
+++ GiftSay/app/src/main/java/activity/lanou3g/com/giftsay/ui/fragment/SelectiveFragment.java
@@ -1,13 +1,23 @@
 package activity.lanou3g.com.giftsay.ui.fragment;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import activity.lanou3g.com.giftsay.R;
 import activity.lanou3g.com.giftsay.modle.net.VolleyResult;
 import activity.lanou3g.com.giftsay.modle.net.VolleyeInstance;
+import activity.lanou3g.com.giftsay.ui.adpter.SelectiveRvAdpter;
 import activity.lanou3g.com.giftsay.ui.app.GiftSayApp;
 
 /**
@@ -16,9 +26,12 @@ import activity.lanou3g.com.giftsay.ui.app.GiftSayApp;
  */
 public class SelectiveFragment extends AbsBaseFragment {
     private TextView showTv;
-
+    private RecyclerView recyclerView;
     private RequestQueue queue;
+    private SelectiveRvAdpter adpter;
+    private List<Integer> datas;
     String dataUrl = "http://api.liwushuo.com/v2/banners";
+
 
     @Override
     protected int setLayoout() {
@@ -27,12 +40,31 @@ public class SelectiveFragment extends AbsBaseFragment {
 
     @Override
     protected void initViews() {
-    showTv = byView(R.id.seletive_tv);
+        showTv = byView(R.id.seletive_tv);
+        recyclerView = byView(R.id.selective_rv);
     }
 
     @Override
     protected void initDatas() {
+         // 获取网络数据的方法
+         getIntent();
+        // 横向图片滑动
+        getTwoRecycleView();
 
+    }
+
+    private void getTwoRecycleView() {  datas = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            datas.add(R.mipmap.ic_launcherss);
+        }
+        adpter = new SelectiveRvAdpter(datas, context);
+        recyclerView.setAdapter(adpter);
+        LinearLayoutManager manager = new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(manager);
+    }
+
+    private void getIntent() {
         queue = Volley.newRequestQueue(GiftSayApp.getContext());
 
         VolleyeInstance.getInstance().startRequest(dataUrl, new VolleyResult() {
